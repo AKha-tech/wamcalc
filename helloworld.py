892a950cb57d70e7d2822a0a057ef236605d1aab
AKha-tech <66359976+AKha-tech@users.noreply.github.com>
2020-06-03 20:02:06 +1000

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -3,4 +3,4 @@ Girls = ['Rish', 'Shenali', 'Banisha', 'Chhavi']
 Rents = ['Dad', 'Mom']
 
 
-print("".join([f'{Guys[i]} is with {Girls[i]}\n' for i in range(len(Guys))]))
+print("".join([f'{Guys[i]} is with {Girls[i]}\n and her {Rents[0]} does not approve' for i in range(len(Guys))]))
