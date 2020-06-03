3da9418c8b0cb8371310e5eaf5b27c1dbb5e7cf3
Chhavi Khathuria <chhavi10@hotmail.com>
2020-06-03 11:26:17 +1000

    Hi

--- helloworld.py
+++ helloworld.py
@@ -4,3 +4,6 @@ Rents = ['Dad', 'Mom']
 
 
 print("".join([f'{Guys[i]} is with {Girls[i]}\n and her {Rents[0]} does not approve' for i in range(len(Guys))]))
+
+aydens_personality =['annoying ','stupid ','beta ','always loses to chhavi']
+print("".join([f'ayden is {aydens_personality[i]}' for i in range(len(aydens_personality))]))
