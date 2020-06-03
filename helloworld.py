c7f368a05a91061fcc191663fa412eda7121d8a5
AKha-tech <66359976+AKha-tech@users.noreply.github.com>
2020-06-03 19:33:49 +1000

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,6 @@
-print('hi')
+Guys = ['Ayden', 'Sohan', 'Kris', 'Sam']
+Girls = ['Rish', 'Shenali', 'Banisha', 'Chhavi']
+Rents = ['Dad', 'Mom']
+
+
+print("".join([f'{Guys[i]} is with {Girls[i]}\n' for i in range(len(Guys))]))
