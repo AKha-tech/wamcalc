d483257f464b03617152af17d65fb9492613b174
AKha-tech <aydenkhalil619@gmail.com>
2020-06-03 21:23:16 +1000

    \n is in wrong place

--- helloworld.py
+++ helloworld.py
@@ -3,4 +3,5 @@ Girls = ['Rish', 'Shenali', 'Banisha', 'Chhavi']
 Rents = ['Dad', 'Mom']
 
 
-print("".join([f'{Guys[i]} is with {Girls[i]}\n and her {Rents[0]} does not approve' for i in range(len(Guys))]))
+print("".join(
+    [f'{Guys[i]} is with {Girls[i]} and her {Rents[0]} does not approve\n' for i in range(len(Guys))]))
